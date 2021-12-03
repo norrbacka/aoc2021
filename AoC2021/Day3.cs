ac08ac69fdcf972c64e47b6c51fde4de4c4092c0
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2021-12-03 12:25:39 +0100

    a little bit more functional

--- AoC2021/Day3.cs
+++ AoC2021/Day3.cs
@@ -6,7 +6,7 @@ public static class Day3
         await Inputs
         .Read("inputs/day3.txt")
         .Select(text => text.Trim().Select(c => c == '1').ToArray())
-        .ToListAsync(); 
+        .ToListAsync();
 
     public static async Task<object> One() =>
          (await GetInput())
@@ -16,7 +16,7 @@ public static class Day3
         (inputs
             .Pipe(Flip)
             .Pipe(GetMostCommonBytes)
-            .Pipe(ToDecimal)) 
+            .Pipe(ToDecimal))
         *
         (inputs
             .Pipe(Flip)
@@ -27,23 +27,23 @@ public static class Day3
         Enumerable.Range(0, original.First().Count())
         .Select(i => original.Select(o => o[i]).ToArray());
 
-    public static bool[] GetMostCommonBytes(IEnumerable<bool[]> inputs) => 
+    public static bool[] GetMostCommonBytes(IEnumerable<bool[]> inputs) =>
         inputs.Select(GetMostCommonBit).ToArray();
 
-    public static bool GetMostCommonBit(bool[] inputs) => 
+    public static bool GetMostCommonBit(bool[] inputs) =>
         inputs.Count(d => d) >= inputs.Count(d => !d);
 
-    public static bool[] GetLeastCommonBytes(IEnumerable<bool[]> inputs) => 
+    public static bool[] GetLeastCommonBytes(IEnumerable<bool[]> inputs) =>
         inputs.Select(GetLeastCommonBit).ToArray();
 
-    public static bool GetLeastCommonBit(bool[] inputs) => 
+    public static bool GetLeastCommonBit(bool[] inputs) =>
         inputs.Count(d => d) < inputs.Count(d => !d);
 
     private static string ToByteString(bool[] inputs) =>
         string.Join("", inputs.Select(i => i ? "1" : "0"));
 
     private static decimal ToDecimal(bool[] inputs) =>
-        Convert.ToInt32(inputs.Pipe(ToByteString),2);
+        Convert.ToInt32(inputs.Pipe(ToByteString), 2);
 
     public static async Task<object> Two() =>
          (await GetInput())
@@ -53,24 +53,25 @@ public static class Day3
         GetOxygenGeneratorRating(inputs) * GetCo2ScrubberRating(inputs);
 
     public static decimal GetOxygenGeneratorRating(IEnumerable<bool[]> inputs, int index = 0) =>
-        Traverse(inputs, (i, x) => i.ElementAt(x), (i, x) => !i.ElementAt(x));
+        Traverse(inputs, true);
 
     public static decimal GetCo2ScrubberRating(IEnumerable<bool[]> inputs, int index = 0) =>
-        Traverse(inputs, (i, x) => !i.ElementAt(x), (i, x) => i.ElementAt(x));
+        Traverse(inputs, false);
 
-    public static decimal Traverse(
-        IEnumerable<bool[]> inputs, 
-        Func<IEnumerable<bool>, int, bool> MoreOnesOrEqFilter, 
-        Func<IEnumerable<bool>, int, bool> MoreZerosFilter,
-        int index = 0)
-    {
-        if (inputs.Count() == 1)
-        {
-            return ToDecimal(inputs.First());
-        }
-        var input = Flip(inputs).ElementAt(index);
-        var moreOnesOrEq = input.Count(i => i) >= input.Count(i => !i);
-        var rest = inputs.Where(x => moreOnesOrEq ? MoreOnesOrEqFilter(x, index) : MoreZerosFilter(x, index));
-        return Traverse(rest, MoreOnesOrEqFilter, MoreZerosFilter, index + 1);
-    }
+    public static decimal Traverse(IEnumerable<bool[]> inputs, bool keepOnes, int index = 0) =>
+        inputs.Count() == 1 ? 
+            ToDecimal(inputs.First()) 
+        :
+            GetIndice(inputs, index)
+            .Pipe(IsMoreOnesOrEqualNumberOfOnesAndZeros)
+            .Pipe(shouldKeepOnes => shouldKeepOnes ? 
+                    inputs.Where(input => input[index] == keepOnes) : 
+                    inputs.Where(input => !input[index] == keepOnes))
+            .Pipe(rest => Traverse(rest, keepOnes, index + 1));
+    
+    private static bool[] GetIndice(IEnumerable<bool[]> inputs, int index) =>
+        Flip(inputs).ElementAt(index);
+
+    private static bool IsMoreOnesOrEqualNumberOfOnesAndZeros(bool[] input) => 
+        input.Count(ones => ones) >= input.Count(zeros => !zeros);
 }
