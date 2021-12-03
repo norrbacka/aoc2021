396660d2e12ea6ab1492b8c12bcc88ca581f4f1b
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2021-12-03 08:57:16 +0100

    Day 3 refactored, not quite happy tho

--- AoC2021/Day3.cs
+++ AoC2021/Day3.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using WinstonPuckett.PipeExtensions;
 
 public static class Day3
 {
@@ -6,105 +6,71 @@ public static class Day3
         await Inputs
         .Read("inputs/day3.txt")
         .Select(text => text.Trim().Select(c => c == '1').ToArray())
-        .ToListAsync();
+        .ToListAsync(); 
 
-    public static async Task<object> One() 
-    {
-        var inputs = await GetInput();
-        var indexes = Enumerable.Range(0, inputs[0].Count());
-        
-        var gammaRateData = new List<string>() { };
-        foreach (var i in indexes) 
-        {
-            var d = inputs.Select(input => input[i]);
-            var zeros = d.Where(d => !d);
-            var ones = d.Where(d => d);
-            gammaRateData.Add(zeros.Count() < ones.Count() ? "1" : "0");
-        }
-        var byteString = string.Join("", gammaRateData);
-        var gammaRate = Convert.ToInt32(byteString, 2);
-        
+    public static async Task<object> One() =>
+         (await GetInput())
+        .Pipe(ComputePowerConsumption);
 
-        var epsilonData = new List<string>() {  };
-        foreach (var j in indexes)
-        {
-            var d = inputs.Select(input => input[j]);
-            var zeros = d.Where(d => !d);
-            var ones = d.Where(d => d);
-            epsilonData.Add(zeros.Count() > ones.Count() ? "1" : "0");
-        }
-        byteString = string.Join("", epsilonData);
-        var epsilon = Convert.ToInt32(byteString, 2);
+    public static decimal ComputePowerConsumption(IEnumerable<bool[]> inputs) =>
+        (inputs
+            .Pipe(Flip)
+            .Pipe(GetMostCommonBytes)
+            .Pipe(ToDecimal)) 
+        *
+        (inputs
+            .Pipe(Flip)
+            .Pipe(GetLeastCommonBytes)
+            .Pipe(ToDecimal));
 
-        return await Task.Run(() => gammaRate * epsilon);
-    }
+    public static IEnumerable<bool[]> Flip(IEnumerable<bool[]> original) =>
+        Enumerable.Range(0, original.First().Count())
+        .Select(i => original.Select(o => o[i]).ToArray());
 
-    public static async Task<object> Two()
-    {
-        var inputs = await GetInput();
+    public static bool[] GetMostCommonBytes(IEnumerable<bool[]> inputs) => 
+        inputs.Select(GetMostCommonBit).ToArray();
 
-        var ogr = GetOgr(inputs, 0);
-        var co2sr = GetC02sr(inputs, 0);
-        return await Task.Run(() => ogr*co2sr);
-    }
+    public static bool GetMostCommonBit(bool[] inputs) => 
+        inputs.Count(d => d) >= inputs.Count(d => !d);
 
-    public static decimal GetOgr(IEnumerable<bool[]> inputs, int index)
-    {
-        if(inputs.Count() == 1)
-        {
-            var byteString = string.Join("", inputs.First().Select(i => i ? "1" : "0"));
-            var ogr = Convert.ToInt32(byteString, 2);
-            return ogr;
-        }
+    public static bool[] GetLeastCommonBytes(IEnumerable<bool[]> inputs) => 
+        inputs.Select(GetLeastCommonBit).ToArray();
 
-        var d = inputs.Select(input => input[index]);
-        var zeros = d.Where(d => !d);
-        var ones = d.Where(d => d);
-        var moreOnes = ones.Count() >= zeros.Count();
-        if (moreOnes)
-        {
-            var toKeep = inputs.Where(input => input[index]);
-            return GetOgr(toKeep, index + 1);
-        }
+    public static bool GetLeastCommonBit(bool[] inputs) => 
+        inputs.Count(d => d) < inputs.Count(d => !d);
 
-        var lessOnes = ones.Count() < zeros.Count();
-        if (lessOnes)
-        {
-            var toKeep = inputs.Where(input => !input[index]);
-            return GetOgr(toKeep, index + 1);
-        }
+    private static string ToByteString(bool[] inputs) =>
+        string.Join("", inputs.Select(i => i ? "1" : "0"));
 
-        throw new InvalidOperationException();
-    }
+    private static decimal ToDecimal(bool[] inputs) =>
+        Convert.ToInt32(inputs.Pipe(ToByteString),2);
 
-    public static decimal GetC02sr(IEnumerable<bool[]> inputs, int index)
+    public static async Task<object> Two() =>
+         (await GetInput())
+        .Pipe(ComputeLifeSupport);
+
+    public static decimal ComputeLifeSupport(IEnumerable<bool[]> inputs) =>
+        GetOxygenGeneratorRating(inputs) * GetCo2ScrubberRating(inputs);
+
+    public static decimal GetOxygenGeneratorRating(IEnumerable<bool[]> inputs, int index = 0) =>
+        Traverse(inputs, (i, x) => i.ElementAt(x), (i, x) => !i.ElementAt(x));
+
+    public static decimal GetCo2ScrubberRating(IEnumerable<bool[]> inputs, int index = 0) =>
+        Traverse(inputs, (i, x) => !i.ElementAt(x), (i, x) => i.ElementAt(x));
+
+    public static decimal Traverse(
+        IEnumerable<bool[]> inputs, 
+        Func<IEnumerable<bool>, int, bool> MoreOnesOrEqFilter, 
+        Func<IEnumerable<bool>, int, bool> MoreZerosFilter,
+        int index = 0)
     {
         if (inputs.Count() == 1)
         {
-            var byteString = string.Join("", inputs.First().Select(i => i ? "1" : "0"));
-            var ogr = Convert.ToInt32(byteString, 2);
-            return ogr;
+            return ToDecimal(inputs.First());
         }
-
-        var d = inputs.Select(input => input[index]);
-        var zeros = d.Where(d => !d);
-        var ones = d.Where(d => d);
-
-        var moreOnes = ones.Count() >= zeros.Count();
-        if (moreOnes)
-        {
-            var zerosToKeep = inputs.Where(input => !input[index]);
-            return GetC02sr(zerosToKeep, index + 1);
-        }
-
-        var lessOnes = ones.Count() < zeros.Count();
-        if (lessOnes)
-        {
-            var onesToKeep = inputs.Where(input => input[index]);
-            return GetC02sr(onesToKeep, index + 1);
-        }
-
-        throw new InvalidOperationException();
+        var input = Flip(inputs).ElementAt(index);
+        var moreOnesOrEq = input.Count(i => i) >= input.Count(i => !i);
+        var rest = inputs.Where(x => moreOnesOrEq ? MoreOnesOrEqFilter(x, index) : MoreZerosFilter(x, index));
+        return Traverse(rest, MoreOnesOrEqFilter, MoreZerosFilter, index + 1);
     }
-
 }
--- README.md
+++ README.md
@@ -12,7 +12,7 @@ dotnet watch I guess?
 ## Progression
 :star::star: [Day 1](/AoC2021/Day1.cs) \
 :star::star: [Day 2](/AoC2021/Day2.cs) \
-:x::x: Day 3 \
+:star::star: [Day 3](/AoC2021/Day3.cs) \
 :x::x: Day 4 \
 :x::x: Day 5 \
 :x::x: Day 6 \
