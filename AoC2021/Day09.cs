a3f093b5835a4cff9fed0f6775f4fb5c708d54d4
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2021-12-09 20:44:33 +0100

    Happy with 9.2 now :)

--- AoC2021/Day09.cs
+++ AoC2021/Day09.cs
@@ -1,107 +1,84 @@
-﻿using System.Diagnostics;
-using System.Reflection;
+﻿using System.Reflection;
 using WinstonPuckett.PipeExtensions;
 
 public static class Day09
 {
+    static IEnumerable<long> Concat(this IEnumerable<long> list, long value) => list.Concat(new[] { value });
+
     private static async Task<long[][]> GetInput() =>
         await Inputs
         .Read(MethodBase.GetCurrentMethod()?.DeclaringType?.FullName?.Split("+").First() ?? "")
         .Select(text => text.Select(i => long.Parse(i.ToString())).ToArray())
         .ToArrayAsync();
 
-    private static IEnumerable<((long x, long y) position, long value, long[] adjacents, bool isLowPolong)> GetData(long[][] input)
-    {
-        for (var i = 0; i < input.Length; i++)
-        for (var j = 0; j < input[i].Length; j++)
-            {
-                var position = (x: i, y: j);
-                var value = input[i][j];
+    record Location((long X, long Y) Position, long Value, long[] Adjacents, bool IsLowerPointer);
 
-                var hasRowAbove = HasTop(i);
-                var hasRowBelow = HasBottom(input, i);
-                var hasColumnLeft = HasLeft(j);
-                var hasColumnRight = HasRight(input, i, j);
-
-                var adjacents = new long[] { }
-                .Pipe(adj => hasRowAbove ? adj.Concat(new[] { input[i - 1][j] }).ToArray() : adj)
-                .Pipe(adj => hasRowBelow ? adj.Concat(new[] { input[i + 1][j] }).ToArray() : adj)
-                .Pipe(adj => hasColumnLeft ? adj.Concat(new[] { input[i][j - 1] }).ToArray() : adj)
-                .Pipe(adj => hasColumnRight ? adj.Concat(new[] { input[i][j + 1] }).ToArray() : adj)
-                .ToArray();
-
-                var isLowPolong = adjacents.All(adj => adj > value);
-                yield return (position, value, adjacents, isLowPolong);
-            }
-    }
-
+    private static IEnumerable<Location> GetLocations(long[][] input) =>
+        Enumerable.Range(0, input.Length).SelectMany(i =>
+        Enumerable.Range(0, input[i].Length).Select(j =>
+        {
+            var position = (x: (long)i, y: (long)j);
+            var value = input[i][j];
+            var adjacents = 
+               (HasTop(i) ? new long[]{GetTop(input, i, j)} : Array.Empty<long>()).Pipe(adj =>
+                HasBottom(input, i) ? adj.Concat(GetBelow(input, i, j)).ToArray() : adj).Pipe(adj =>
+                HasLeft(j) ? adj.Concat(GetLeft(input, i, j)).ToArray() : adj).Pipe(adj =>
+                HasRight(input, i, j) ? adj.Concat(GetRight(input, i, j)).ToArray() : adj)
+            .ToArray();
+            var isLowerPointer = adjacents.All(adj => adj > value);
+            return new Location(position, value, adjacents, isLowerPointer);
+        }));
 
     private static bool HasTop(long i) => i > 0;
     private static bool HasRight(long[][] input, long i, long j) => j < (input[i].Length - 1);
     private static bool HasBottom(long[][] input, long i) => i < (input.Length - 1);
     private static bool HasLeft(long j) => j > 0;
+    private static long GetTop(long[][] input, long i, long j) => input[i - 1][j];
+    private static long GetRight(long[][] input, long i, long j) => input[i][j + 1];
+    private static long GetBelow(long[][] input, long i, long j) => input[i + 1][j];
+    private static long GetLeft(long[][] input, long i, long j) => input[i][j - 1];
+    private static bool TopIsHigherLocation(long[][] input, long i, long j, long value) => GetTop(input, i, j) > value;
+    private static bool RightIsHigherLocation(long[][] input, long i, long j, long value) => GetRight(input, i, j) > value;
+    private static bool BelowIsHigherLocation(long[][] input, long i, long j, long value) => GetBelow(input, i, j) > value;
+    private static bool LeftIsHigherLocation(long[][] input, long i, long j, long value) => GetLeft(input, i, j) > value;
 
+    public static async Task<object> One() =>
+        (await GetInput())
+        .Pipe(GetLocations)
+        .Pipe(inputAndAdjacents => inputAndAdjacents.Where(x => x.IsLowerPointer))
+        .Sum(x => (x.Value + 1));
 
-    public static async Task<object> One()
+    static IEnumerable<(long i, long j)> GetBasin(long[][] input, long i, long j, IEnumerable<(long i, long j)> sizes)
     {
-        var input = await GetInput();
-        var inputAndAdjacents = GetData(input);
-        var lowPolongs = inputAndAdjacents.Where(x => x.isLowPolong).ToArray();
-        var sum = lowPolongs.Sum(x => x.value + 1);
-        return sum;
-    }
+        if (sizes.Contains((i, j)) && sizes.Count() > 1) return sizes;
 
-    static IEnumerable<(long i, long j)> BasinSize(long[][] input, long i, long j, IEnumerable<(long i, long j)> sizes)
-    {
-        if(sizes.Contains((i,j)) && sizes.Count() > 1)
-        {
-            return sizes;
-        }
         sizes = sizes.ToList().Concat(new[] { (i, j) });
         var value = input[i][j];
 
-        var t = HasTop(i);
-        var r = HasRight(input, i, j);
-        var b = HasBottom(input, i);
-        var l = HasLeft(j);
+        var GoUp = HasTop(i) && TopIsHigherLocation(input, i, j, value) && GetTop(input, i, j) != 9;
+        if (GoUp) sizes = GetBasin(input, i - 1, j, sizes);
 
-        if (t && (input[i - 1][j] > value && input[i - 1][j] != 9))
-        {
-            sizes = BasinSize(input, i - 1, j, sizes);
-        }
-        if (b && (input[i + 1][j] > value) && (input[i + 1][j] != 9))
-        {
-            sizes = BasinSize(input, i + 1, j, sizes);
-        }
-        if (l && (input[i][j - 1] > value) && input[i][j - 1] != 9)
-        {
-            sizes = BasinSize(input, i, j - 1, sizes);
-        }
-        if (r && (input[i][j + 1] > value) && input[i][j + 1] != 9)
-        {
-            sizes = BasinSize(input, i, j + 1, sizes);
-        }
+        var GoDown = HasBottom(input, i) && BelowIsHigherLocation(input, i, j, value) && GetBelow(input, i, j) != 9;
+        if (GoDown) sizes = GetBasin(input, i + 1, j, sizes);
+
+        var GoLeft = HasLeft(j) && LeftIsHigherLocation(input, i, j, value) && GetLeft(input, i, j) != 9;
+        if (GoLeft) sizes = GetBasin(input, i, j - 1, sizes);
+
+        var GoRight = HasRight(input, i, j) && RightIsHigherLocation(input, i, j, value) && GetRight(input, i, j) != 9;
+        if (GoRight) sizes = GetBasin(input, i, j + 1, sizes);
 
         return sizes;
     }
 
-    public static async Task<object> Two()
-    {
-        var input = await GetInput();
-        var inputAndAdjacents = GetData(input);
-        var lowPolongs = inputAndAdjacents.Where(x => x.isLowPolong).ToArray();
-        var basins = lowPolongs.Select(l => 
-            BasinSize(input, l.position.x, l.position.y, new (long, long)[] { (l.position.x, l.position.y) })
-            .Distinct()
-        ).ToArray();
-        var top3largetsBasins = 
-            basins
-            .OrderByDescending(y => y.Count())
-            .Take(3)
-            .ToArray();
-        var basinsSum =
-            top3largetsBasins
-            .Aggregate(1, (x, y) => x * y.Count());
-        return basinsSum;
-    }
+    public static async Task<object> Two() =>
+        (await GetInput())
+        .Pipe(input => 
+            GetLocations(input)
+            .Where(x => x.IsLowerPointer)
+            .Select(l =>
+                GetBasin(input, l.Position.X, l.Position.Y, new (long, long)[] { (l.Position.X, l.Position.Y) })
+                .Distinct()))
+        .OrderByDescending(y => y.Count())
+        .Take(3)
+        .Aggregate(1, (x, y) => x * y.Count());
 }
