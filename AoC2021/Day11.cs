741c1464b3f1a8787fece4ab6096895ac6d38c5a
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2021-12-11 14:27:32 +0100

    Cleaning up

--- AoC2021/Day11.cs
+++ AoC2021/Day11.cs
@@ -1,37 +1,38 @@
-﻿using System.Diagnostics;
-using System.Reflection;
+﻿using System.Reflection;
 
 public static class Day11
 {
     private static async Task<int[][]> GetInput() =>
         await Inputs
         .Read(MethodBase.GetCurrentMethod()?.DeclaringType?.FullName?.Split("+").First() ?? "")
-        .Select(text => text.Select(i => int.Parse(i.ToString())).ToArray())
+        .Select(teXt => teXt.Select(i => int.Parse(i.ToString())).ToArray())
         .ToArrayAsync();
 
-    record Octo(int rowLength, int colLength, int x, int y, int energy)
+    record Octo(int RowCount, int ColCount, int X, int Y, int Energy)
     {
-        public Octo SetEnergy(int e) => e >= 0 && e <= 10 ? new Octo(rowLength, colLength, x, y, e) : this;
+        public Octo SetEnergy(int energy) => 
+            energy >= 0 && energy <= 10 ? 
+                new Octo(RowCount, ColCount, X, Y, energy) : this;
 
-        public bool Flashes => energy > 9;
+        public bool Flashes => Energy > 9;
 
-        public bool HasTop => y > 0;
-        public bool HasRight => x < (colLength - 1);
-        public bool HasBottom => y < (rowLength - 1);
-        public bool HasLeft => x > 0;
+        public bool HasTop => Y > 0;
+        public bool HasRight => X < (ColCount - 1);
+        public bool HasBottom => Y < (RowCount - 1);
+        public bool HasLeft => X > 0;
         public bool HasTopRight => HasTop && HasRight;
         public bool HasTopLeft => HasTop && HasLeft;
         public bool HasBottomRight => HasBottom && HasRight;
         public bool HasBottomLeft => HasBottom && HasLeft;
 
-        public Octo GetTop(Octo[][] octos) => octos[y - 1][x];
-        public Octo GetTopRight(Octo[][] octos) => octos[y - 1][x + 1];
-        public Octo GetRight(Octo[][] octos) => octos[y][x + 1];
-        public Octo GetBottomRight(Octo[][] octos) => octos[y + 1][x + 1];
-        public Octo GetBottom(Octo[][] octos) => octos[y + 1][x];
-        public Octo GetBottomLeft(Octo[][] octos) => octos[y + 1][x - 1];
-        public Octo GetLeft(Octo[][] octos) => octos[y][x - 1];
-        public Octo GetTopLeft(Octo[][] octos) => octos[y - 1][x - 1];
+        public Octo GetTop(Octo[][] octos) => octos[Y - 1][X];
+        public Octo GetTopRight(Octo[][] octos) => octos[Y - 1][X + 1];
+        public Octo GetRight(Octo[][] octos) => octos[Y][X + 1];
+        public Octo GetBottomRight(Octo[][] octos) => octos[Y + 1][X + 1];
+        public Octo GetBottom(Octo[][] octos) => octos[Y + 1][X];
+        public Octo GetBottomLeft(Octo[][] octos) => octos[Y + 1][X - 1];
+        public Octo GetLeft(Octo[][] octos) => octos[Y][X - 1];
+        public Octo GetTopLeft(Octo[][] octos) => octos[Y - 1][X - 1];
     };
 
     static Octo[][] ToOcto(this int[][] input) { 
@@ -39,19 +40,19 @@ public static class Day11
             Enumerable.Range(0, input.Length).SelectMany(y =>
             Enumerable.Range(0, input[y].Length).Select(x => 
                 new Octo(input.Length, input[y].Length, x, y, input[y][x])))
-            .ToLookup(o => o.y);
-        var rows = octosList.Select(x => x.Key);
+            .ToLookup(o => o.Y);
+        var rows = octosList.Select(X => X.Key);
         var cols = rows.Select(r => octosList[r].ToArray()).ToArray();
         return cols;
     }
 
-    static (Octo[][], (int y, int x)[]) Step(Octo[][] octos, Octo octo, (int y, int x)[] flashed)
+    static (Octo[][], (int Y, int X)[]) Step(Octo[][] octos, Octo octo, (int Y, int X)[] flashed)
     {
-        octo = octo.SetEnergy(octo.energy + 1);
-        octos[octo.y][octo.x] = octo;
-        if (octo.Flashes && !flashed.Contains((octo.y, octo.x)))
+        octo = octo.SetEnergy(octo.Energy + 1);
+        octos[octo.Y][octo.X] = octo;
+        if (octo.Flashes && !flashed.Contains((octo.Y, octo.X)))
         {
-            flashed = flashed.Append((octo.y, octo.x)).ToArray();
+            flashed = flashed.Append((octo.Y, octo.X)).ToArray();
             if (octo.HasTop) (octos, flashed) = Step(octos, octo.GetTop(octos), flashed);
             if(octo.HasTopRight) (octos, flashed) = Step(octos, octo.GetTopRight(octos), flashed);
             if(octo.HasRight) (octos, flashed) = Step(octos, octo.GetRight(octos), flashed);
@@ -67,37 +68,24 @@ public static class Day11
     static (Octo[][] Octos, int TotalFlashes) Simulate(this Octo[][] octos, int step, int stop)
     {
         int totalFlashes = 0;
-        octos.Print(0);
         do
         {
-            var flashed = new (int y, int x)[] { };
-            for (int y = 0; y < octos.Length; y++)
+            var flashed = new (int Y, int X)[] { };
+            for (int Y = 0; Y < octos.Length; Y++)
             {
-                for (int x = 0; x < octos[y].Length; x++)
+                for (int X = 0; X < octos[Y].Length; X++)
                 {
-                    (octos, flashed) = Step(octos, octos[y][x], flashed);
+                    (octos, flashed) = Step(octos, octos[Y][X], flashed);
                 }
             }
             totalFlashes += flashed.Count();
             foreach (var flash in flashed)
             {
-                octos[flash.y][flash.x] = octos[flash.y][flash.x].SetEnergy(0);
+                octos[flash.Y][flash.X] = octos[flash.Y][flash.X].SetEnergy(0);
             }
-            octos.Print(step);
         }
         while (++step <= stop); 
         return (octos, totalFlashes);
-    }
-
-    static void Print(this Octo[][] octos, int step)
-    {
-        Debug.WriteLine(step == 0 ? "Before any steps:" : $"After step {step}:");
-        for (int y = 0; y < octos.Length; y++)
-        {
-            var numbers = string.Join("", octos[y].Select(o => o.energy));
-            Debug.WriteLine(numbers);
-        }
-        Debug.WriteLine("");
     }
 
     public static async Task<object> One() =>
@@ -107,24 +95,24 @@ public static class Day11
         .TotalFlashes;
 
     static bool AllHasFlashed(this Octo[][] octos) => 
-        octos.SelectMany(o => o).All(o => o.energy == 0);
+        octos.SelectMany(o => o).All(o => o.Energy == 0);
 
     static int SimulateUntilAllFlashes(this Octo[][] octos)
     {
         int step = 0;
         do
         {
-            var flashed = new (int y, int x)[] { };
-            for (int y = 0; y < octos.Length; y++)
+            var flashed = new (int Y, int X)[] { };
+            for (int Y = 0; Y < octos.Length; Y++)
             {
-                for (int x = 0; x < octos[y].Length; x++)
+                for (int X = 0; X < octos[Y].Length; X++)
                 {
-                    (octos, flashed) = Step(octos, octos[y][x], flashed);
+                    (octos, flashed) = Step(octos, octos[Y][X], flashed);
                 }
             }
             foreach (var flash in flashed)
             {
-                octos[flash.y][flash.x] = octos[flash.y][flash.x].SetEnergy(0);
+                octos[flash.Y][flash.X] = octos[flash.Y][flash.X].SetEnergy(0);
             }
             step++;
         }
